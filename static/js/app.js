11e6706d5255ae11c2af0d7374203d164b5c37ad
Hernan Lozano <hernantz@gmail.com>
2013-06-20 20:54:40 -0300

    Implementing client side poll logic.

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,52 @@
 var app = {}
+window.app = app;
 
 app.TrackModel = Backbone.Model.extend({
   defaults: {
-    'artist': ''
-    'track': ''
-    'duration': ''
+    'artist': '',
+    'track': '',
+    'duration': '',
+    'upvotes': 0
   },
 });
 
 app.TrackCollection = Backbone.Collection.extend({
   model: app.TrackModel
+});
+
+app.SearchTracksView = Backbone.View.extend({
+  initialize: function(options) {
+    this.$el = $('#searchlist');
+    this.collection = options.collection
+    this.collection.on('add', this.render, this);
+    this.collection.on('reset', this.render, this);
+    this.getTracks();
+  },
+  getTracks: function() {
+    var self = this;
+    $.getJSON('/get_tracks', function(response){
+      _.each(response, function(model) {
+        self.collection.add(model);
+      });
+    });
+  },
+  render: function() {
+    var tpls = "";
+    this.collection.each(function(model) {
+      tpls += _.template('<li><a href="docs/api/themes.html"><%= artist %> - <%= track %></a></li>', model.toJSON());
+    });
+    this.$el.html(tpls); 
+  }
+});
+
+app.NowPlayingView = Backbone.View.extend({ });
+
+app.init = function() {
+  var trackCollection = new app.TrackCollection();
+  new app.SearchTracksView({collection: trackCollection});
+}
+
+
+$(document).ready(function() {
+  app.init();
 });
