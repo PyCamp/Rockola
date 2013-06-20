2a282533575b4d3336cd49c56f0453574f14105a
jtrad <you@example.com>
2013-06-20 18:12:56 -0300

    Definiciones del votesengine

---
+++ votesengine/votesengine.py
@@ -0,0 +1,23 @@
+
+def play_new_song():
+    """se usa para avisarle a shiva que reproduzca una nueva cancion"""
+    raise NotImplementedError()
+
+
+# instancia el VotesManager
+
+# read data from rabbitMQ
+
+# parse json
+
+# update track list
+
+# Verifica la Operacion, por ahora soporta Votos y Nueva cancion
+
+# Si Votos:
+# Genera las dos listas Top y last
+
+# Pushea top y last a RabitMQ???
+
+
+#¿Almacena cada tanto en un sqlite?
