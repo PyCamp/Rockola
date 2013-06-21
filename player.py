2dd1dde1722f1af903fe94b75f5eaa9e3c51eeb8
Diego Mascialino <dmascialino@gmail.com>
2013-06-21 18:33:26 -0300

    Added remaining_time and watcher to VLCController

--- player.py
+++ player.py
@@ -1,10 +1,13 @@
 
 import json
+import time
+
+from multiprocessing import Process
 from subprocess import Popen
 from urllib import urlencode
 from urllib2 import urlopen
 
-
+from msgs_queue import queue_manager
 
 class VLCController(object):
 
@@ -18,7 +21,9 @@ class VLCController(object):
                         '-I http',
                         '--http-host', '0.0.0.0',
                         '--http-port', self.PORT])
-        self.base_url = 'http://localhost:%s/requests/status.xml' % self.PORT
+        self.base_url = 'http://localhost:%s/requests/status.json' % self.PORT
+        self.wd = NextSongWatcher()
+        self.wd.run()
 
     def add_song(self, path, play_it_now=False):
         '''
@@ -29,8 +34,13 @@ class VLCController(object):
         s = '%s?%s' % (self.base_url, urlencode(data))
         urlopen(s)
 
-    def get_player_status(self):
-        pass
+    def get_reamining_time(self):
+        status_raw = urlopen(self.base_url).read()
+        status = json.loads(status_raw)
+        if 'time' in status:
+            return status['length'] - status['time']
+        else:
+            return 0
 
 
 class ShivaClient(object):
@@ -52,7 +62,7 @@ class ShivaClient(object):
         return json.loads(data)
 
     def get_tracks(self, ids):
-        
+
         tracks = self._request('tracks')
 
         response = {}
@@ -71,6 +81,33 @@ class ShivaClient(object):
 
     def get_artists(self):
         return self._request('artists')
+
+class NextSongWatcher(object):
+
+    def __init__(self, vlcc):
+        self.control_name = queue_manager.get_queue_name('control')
+        self.running = True
+        self.sender = queue_manager.Queue()
+        self.vlcc = vlcc
+
+    def receive_loop(self):
+        while self.running:
+            remaining_time = self.vlcc.remaining_time()
+            print remaining_time
+            if remaining_time < 10:
+                data = {'timestamp': int(time.time()),
+                        'operation': 'nueva_cancion'}
+                self.sender.send(self.control_name, json.dumps(data))
+                time.sleep(20)
+
+            time.sleep(5)
+
+    def run(self):
+        p = Process(target=self.receive_loop)
+        p.start()
+
+    def stop(self):
+        self.stopping = False
 
 if __name__ == '__main__':
     pass
