1e03dc325a3acbdd0018446d1c42936a07042bd4
Raspberry Pi <pi@raspberry>
2013-06-21 20:48:41 +0000

    Fixes to the Rockola script

--- Rockola.sh
+++ Rockola.sh
@@ -9,8 +9,10 @@ else
   tmux new-window -t rockola:2 -n shiva
 fi
 
-tmux send-keys -t rockola:0 "source bin/activate; twistd -n cyclone-sse --amqp-host=192.168.10.90 -l 0" C-m
-tmux send-keys -t rockola:1 "source bin/activate; python app.py" C-m
 tmux send-keys -t rockola:2 "cd ~/shiva-server; source .virtualenv/bin/activate; shiva-server" C-m
+tmux send-keys -t rockola:0 "source bin/activate; twistd -n cyclone-sse --amqp-host=192.168.10.90 -l 0" C-m
+sleep 2
+tmux send-keys -t rockola:1 "source bin/activate; python app.py" C-m
 
+tmux select-window -t rockola:1
 tmux attach-session -d -t rockola
