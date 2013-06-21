a3e60170604b288b5ec7c96b5c442badab6e56ff
Lucia Prado <prado.lucia1@gmail.com>
2013-06-21 18:47:14 -0300

    Run flask app inside gunicorn in Rockola.sh

--- Rockola.sh
+++ Rockola.sh
@@ -18,7 +18,7 @@ fi
 tmux send-keys -t rockola:2 "cd ~/shiva-server; source .virtualenv/bin/activate; shiva-server" C-m
 tmux send-keys -t rockola:0 "source bin/activate; twistd -n cyclone-sse --amqp-host=192.168.10.90 -l 0" C-m
 sleep 2
-tmux send-keys -t rockola:1 "source bin/activate; python app.py" C-m
+tmux send-keys -t rockola:1 "source bin/activate; gunicorn -k gevent -b 0.0.0.0:5000 --debug app:app" C-m
 tmux send-keys -t rockola:3 "source bin/activate; cd votesengine; python votesengine.py" C-m
 
 tmux select-window -t rockola:1
