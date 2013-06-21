3a62056976c1b9d7f3ee436587e3bbd48fd8461a
jtrad <you@example.com>
2013-06-21 00:05:23 -0300

    votesengine emitiendo la lista de top y last

--- msgs_queue/test_receive.py
+++ msgs_queue/test_receive.py
@@ -1,11 +1,11 @@
 import queue_manager
+while True:
+    control_name = queue_manager.get_queue_name('control')
+    lists_name = queue_manager.get_queue_name('lists')
 
-control_name = queue_manager.get_queue_name('control')
-lists_name = queue_manager.get_queue_name('lists')
-
-cmd_receiver = queue_manager.Receiver(control_name)
-lists_receiver = queue_manager.Receiver(lists_name)
+    #cmd_receiver = queue_manager.Receiver(control_name)
+    lists_receiver = queue_manager.Receiver(lists_name)
 
 
-print cmd_receiver.receive()
-print lists_receiver.receive()
+    #print cmd_receiver.receive()
+    print lists_receiver.receive()
